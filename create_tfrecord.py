2aada8e90ddb5087514c77ee7b266557e6098fe7
Clément Petit <petitc@student.chalmers.se>
2020-12-10 08:09:42 +0100

    put object detection back in models/research

--- create_tfrecord.py
+++ create_tfrecord.py
@@ -20,6 +20,7 @@ required arguments:
 import os
 import argparse
 import io
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 import tensorflow.compat.v1 as tf
 from PIL import Image
 from object_detection.utils import dataset_util, label_map_util
